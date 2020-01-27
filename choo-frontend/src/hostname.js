8f5d84a9023bcfdf6de380b53b8dd2375271c41d
Osaou <osaourell@gmail.com>
2020-01-27 01:57:54 +0100

    Refactor hostname client code to support generic domain name deployments

--- choo-frontend/src/hostname.js
+++ choo-frontend/src/hostname.js
@@ -1,10 +1,11 @@
 const getWsUrl = () => {
-    if (window.location.hostname.includes("richodemus.com")) {
-        console.log("using ws hostname: api.choo.richodemus.com");
-        return "wss://api.choo.richodemus.com"
+    if (window.location.hostname.includes("localhost")) {
+        console.log("using ws hostname: ", window.location.hostname + ":8080");
+        return "ws://" + window.location.hostname + ":8080"
     }
-    console.log("using ws hostname: ", window.location.hostname + ":8080");
-    return "ws://" + window.location.hostname + ":8080"
+
+    console.log("using ws hostname: api.", window.location.hostname);
+    return "wss://api." + window.location.hostname
 };
 
 export {getWsUrl}
